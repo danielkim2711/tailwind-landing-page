eb068e378c1b25d2c3c43304bf0e6aa63f798091
Daniel Kim <danielkim2711@gmail.com>
2022-07-21 21:13:51 +1200

    Add Hero component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Hero />
     </BrowserRouter>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+
+import illustrationIntro from '../assets/images/illustration-intro.svg';
+
+const Hero = () => {
+  return (
+    <section id='hero'>
+      <div className='container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row'>
+        <div className='flex flex-col mb-32 space-y-12 md:w-1/2'>
+          <h1 className='max-w-md text-4xl font-bold text-center md:text-5xl md:text-left'>
+            Bring everyone together to build better products
+          </h1>
+          <p className='max-w-sm text-center text-darkGrayishBlue md:text-left'>
+            Manage makes it simple for software teams to plan day-to-day tasks
+            while keeping the larger team goals in view.
+          </p>
+          <div className='flex justify-center md:justify-start'>
+            <Link
+              to='#'
+              className='p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight'
+            >
+              Get Started
+            </Link>
+          </div>
+        </div>
+
+        <div className='md:w-1/2'>
+          <img src={illustrationIntro} alt='' />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
